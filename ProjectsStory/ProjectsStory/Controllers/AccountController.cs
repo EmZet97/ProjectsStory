d39b73455a53053d2c968c8aef47bc27dd0053ca
MattShah <mzawiski97@gmail.com>
2020-02-23 19:17:16 +0100

    First EF Code First structure

---
+++ ProjectsStory/ProjectsStory/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using ProjectsStory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectsStory.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserContext context = new UserContext();
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            user.Avatar = "Default.png";            
+
+            if(context.Users.FirstOrDefault(v => v.Email.ToLower() == user.Email.ToLower()) == null)
+            {
+                context.Users.Add(user);
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch(NullReferenceException ex)
+                {
+                    ViewBag.ErrorMessage = "Unknow Error. Please try again";
+                    return View(user);
+                }
+
+                return RedirectToRoute("Login");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Email already exists";
+                return View(user);
+            }
+            
+        }
+    }
+}
---
+++ ProjectsStory/ProjectsStory/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjectsStory.Models
+{
+    [Table(name: "User")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        public string Surname { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Nick { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Avatar { get; set; } = "Default.png";
+
+
+    }
+}
