8e638e915ec7d20622482376fb77fea36bb5d26c
MattShah <mzawiski97@gmail.com>
2020-02-24 17:39:12 +0100

    Continue login/register part
    Pasword hashing to do

--- ProjectsStory/ProjectsStory/Controllers/AccountController.cs
+++ ProjectsStory/ProjectsStory/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace ProjectsStory.Controllers
@@ -17,9 +18,13 @@ namespace ProjectsStory.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Register(User user)
         {
-            user.Avatar = "Default.png";            
+            user.Avatar = "Default.png";
+            //Hashing password
+            string pass = Crypto.SHA256(user.Password);
+            user.Password = pass;
 
             if(context.Users.FirstOrDefault(v => v.Email.ToLower() == user.Email.ToLower()) == null)
             {
@@ -27,6 +32,10 @@ namespace ProjectsStory.Controllers
 
                 try
                 {
+                    //User urr = context.Users.FirstOrDefault(v => v.Email.ToLower() == "mzawiski97@gmail.com");
+                    //Hashing password
+                    //string passs = Crypto.SHA256(urr.Password);
+                    //urr.Password = passs;
                     context.SaveChanges();
                 }
                 catch(NullReferenceException ex)
@@ -43,6 +52,47 @@ namespace ProjectsStory.Controllers
                 return View(user);
             }
             
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User usr = context.Users.Where(a => a.Email.Equals(user.Email)).FirstOrDefault();
+                
+                if (usr != null)
+                {
+                    bool passCorrect = Crypto.VerifyHashedPassword(usr.Password, user.Password);
+                    if (passCorrect)
+                    {
+                        Session.Clear();
+                        Session["id"] = usr.UserId;
+                        Session["email"] = usr.Email;
+                        Session["avatar"] = usr.Avatar;
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Incorrect password";
+                        return View(user);
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Incorrect email";
+                    return View(user);
+                }
+            }
+            return View(user);
+
         }
     }
 }
